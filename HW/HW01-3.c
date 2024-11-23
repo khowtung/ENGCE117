7f5cbdec55bfb1bbdf5947a95d5e3fa8ebd3ee0f
khowtung <tttt3908gmail.com>
2024-11-23 13:04:59 +0700

    hw013 done

---
+++ HW/HW01-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ( ) {
+
+    char name [ 100 ] ;
+    char lastname [ 100 ] ;
+    printf( " please enter name " ) ;
+    scanf( "%s" , name ) ;
+    printf( " please enter lastname " ) ;
+    scanf( "%s" , lastname ) ;
+
+    printf( "%s %s , TC , RMUTL , Chiang Mai , Thailand " , name , lastname ) ;
+
+    return 0 ;
+}
