df41cca5cf6fb7f242822b986fcc29ea420b1aac
khowtung <tttt3908gmail.com>
2024-12-08 21:37:58 +0700

    064

---
+++ HW/HW06/HW064/HW064.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main( ) {
+
+    int number , num = 9 , sum = 0 ;
+    printf( "Enter number :" ) ;
+    scanf( " %d" , &number ) ;
+
+    printf( "Series = " ) ;
+    for (int  i = 0 ; i < number ; i++ ) {
+        printf( "%d" , num ) ;
+        sum += num ;
+        num = num * 10 + 9 ;
+        printf( " + " ) ;
+    } // end for
+
+    printf( "\nSum = %d" , sum ) ;
+
+    return 0 ;
+} // end function
