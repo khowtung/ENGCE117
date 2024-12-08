9b77505a0d88890f7e501d46d9092c7bf52c6807
khowtung <tttt3908gmail.com>
2024-12-08 18:23:24 +0700

    058again

--- HW/HW05/HW058/HW058.c
+++ HW/HW05/HW058/HW058.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main() {
+int main( ) {
     int num1 , num2 , num3 ;
 
     // รับค่าจากผู้ใช้
-    printf( "Enter three numbers: " ) ;
     scanf( "%d %d %d" , &num1 , &num2 , &num3 ) ;
 
     // แสดงผลลำดับตัวเลข
