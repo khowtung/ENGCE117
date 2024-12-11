1be088a9308dfb2e1b7ebb7f73ecee29dc972521
khowtung <tttt3908gmail.com>
2024-12-11 20:36:36 +0700

    072

---
+++ HW/HW07/HW072/hw072.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ( ) {
+
+    int numstart , numend , answer ;
+    int i ;
+
+    printf( "start : " ) ;
+    scanf( "%d" , &numstart ) ;
+    printf( "end : " ) ;
+    scanf( "%d" , &numend ) ;
+
+    while ( numstart <= numend ) {
+        i = 1 ;
+        while ( i < 10 ) {
+            answer = numstart * i ;
+            printf( "%d x %d = %d\n" , numstart , i , answer ) ;
+            i++ ;
+        } // end for เอาไว้คูณเลข และแสดงเลขที่คูณถุงแม่ต
+        numstart++ ;
+    }
+    
+    return 0 ;
+} // end function 
