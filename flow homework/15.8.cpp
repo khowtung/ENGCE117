f0759c3bdd5c5885eb2b9112115a23bdb52cf15f
khowtung <tttt3908gmail.com>
2025-03-08 00:55:18 +0700

    www

--- flow homework/15.8.cpp
+++ flow homework/15.8.cpp
@@ -16,3 +16,107 @@
         Sort (A to Z)  : Ant, Bird, Cobra, Mango, Orange, Thai, Wolf, Yoyo, Zero .
         Sort (Z to A)  : Zero, Yoyo, Wolf, Thai, Orange, Mango, Cobra, Bird, Ant .
 */
+// #include <stdio.h>
+
+// void readtext ( ) ;
+
+// int main( ) {
+//     readtext( ) ;
+//     return 0 ;
+// } //end main
+
+// void readtext ( ) {
+
+//     char name[ 100 ][ 5 ] ;
+
+//     FILE *fp ;
+//     fp = fopen( "a15-8.txt" , "r" ) ;
+//     if ( fp == NULL ) {
+//         printf( " error " ) ;
+//     } //end if
+
+//     int count = 10 ;
+//     for (int i = 0 ; i < count ; i++) {
+//         for (int j = 0 ; j < count ; j++) {
+//              if ( fscanf( fp , " %s" , name[ i ] ) == EOF ) {
+//             break ;
+//         } //end for
+//         // printf( " %s" , name[ i ] ) ;
+//         printf( " %c" , name[ i ][ 0 ] ) ;
+//         } //end for
+        
+//     }
+//     fclose( fp ) ;
+    
+// }
+
+#include <stdio.h>
+#include <string.h>
+
+void readtext( ) ;
+void sortWords( char words[ 20 ][ 20 ] , int count , int ascending ) ;
+
+int main( ) {
+    readtext( ) ;
+    return 0 ;
+}
+
+void readtext( ) {
+    char words[ 20 ][ 20 ] ;
+    int count = 0 ;
+    
+    FILE *fp = fopen( "a15-8.txt" , "r" ) ;
+    if ( fp == NULL ) {
+        printf( "error\n" ) ;
+        return ;
+    }
+    
+    while ( count < 20 && fscanf( fp , "%19s" , words[ count ] ) != EOF ) {
+        count++ ;
+    }
+    fclose( fp ) ;
+    
+    // Display normal order
+    printf( "Normal ( %d Item ) : " , count ) ;
+    for ( int i = 0 ; i < count ; i++ ) {
+        printf( "%s" , words[ i ] ) ;
+        if ( i < count - 1 ) printf( ", " ) ;
+    }
+    printf( " .\n" ) ;
+    
+    // Sort A to Z
+    sortWords( words , count , 1 ) ;
+    printf( "Sort (A to Z)  : " ) ;
+    for ( int i = 0 ; i < count ; i++ ) {
+        printf( "%s" , words[ i ] ) ;
+        if ( i < count - 1 ) printf( ", " ) ;
+    }
+    printf( " .\n" ) ;
+    
+    // Sort Z to A
+    sortWords( words , count , 0 ) ;
+    printf( "Sort (Z to A)  : " ) ;
+    for ( int i = 0 ; i < count ; i++ ) {
+        printf( "%s" , words[ i ] ) ;
+        if ( i < count - 1 ) printf( ", " ) ;
+    }
+    printf( " .\n" ) ;
+}
+
+void sortWords( char words[ 20 ][ 20 ] , int count , int ascending ) {
+    char temp[ 20 ] ;
+    for ( int i = 0 ; i < count - 1 ; i++ ) {
+        for ( int j = i + 1 ; j < count ; j++ ) {
+            if ( ( ascending && strcmp( words[ i ] , words[ j ] ) > 0 ) || ( !ascending && strcmp( words[ i ] , words[ j ] ) < 0 ) ) {
+                strcpy( temp , words[ i ] ) ;
+                strcpy( words[ i ] , words[ j ] ) ;
+                strcpy( words[ j ] , temp ) ;
+            }
+        }
+    }
+}
+
+
+
+
+
--- flow homework/15.9.cpp
+++ flow homework/15.9.cpp
@@ -7,3 +7,76 @@
     Output:
         Output: I AM GROOT
 */
+
+// #include <stdio.h>
+// #include<stdlib.h>
+
+// void readtext ( ) ;
+
+// int main( ) {
+//     readtext( ) ;
+//     return 0 ;
+// }
+
+// void readtext ( ) {
+//     FILE *fp ;
+//     fp = fopen ( "a15-9.txt" , " r " ) ;
+//     if ( fp = NULL ) {
+//         printf( " Can't open file! " ) ; exit( 0 ) ;
+//     }
+//     char name[ 20 ] ;
+
+//     for (int  i = 0 ; i < 20 ; i++ ) {
+//         if ( fscanf( fp , " %c" , name )  == EOF )
+//             break ;
+//         printf( " = %c" , name ) ;
+//     }
+//     fclose( fp ) ;
+// }
+
+#include <stdio.h>
+
+void readtext ( ) ;
+void reverse ( int name ) ;
+
+int main ( ) {
+    readtext( ) ;
+    return 0 ;
+}//end main
+
+void readtext ( ) {
+
+        FILE *fp ;
+    fp = fopen( "a15-9.txt" , "r" ) ;
+    if ( fp == NULL ) {
+        printf( " ERR " ) ;
+    }//end if err
+
+    char name ;
+    int num ;
+    
+    while ( ( name = fgetc( fp ) ) != EOF ) {
+        num = name ;
+        reverse ( num ) ;
+        // printf( " %d" , name ) ;
+        // printf( " %d " , num ) ;
+    } // end while
+    fclose( fp ) ;
+
+}
+
+void reverse ( int num ) {
+    num += 6 ;
+    if ( num >= 65 && num <= 90 ) {
+        printf( " %c" , num ) ;
+    } else if ( num > 90 ){
+        num = 65 ; 
+        printf( " %c" , num ) ;
+    } else {
+        printf( " " ) ;
+    }
+    // printf( " %c" , num ) ;
+
+
+
+}
